551ca41fd1a78754d57b93d05b053a8599eec5a0
ryudaibaba <1844ryudai@gmail.com>
2021-04-07 00:33:10 +0900

    #7

---
+++ resources/js/components/TodayComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">今日の日付</div>
+
+          <div class="card-body">
+            {{ day }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        day: '0:0',
+      }
+    },
+    mounted() {
+      let d = new Date();
+      this.day = d.getFullYear().toString() + "/" + (d.getMonth()+1).toString() + "/" + d.getDate().toString();
+    }
+  }
+</script>
